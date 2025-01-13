a49985d73d747c8e86de1bbbe2d77617d51ca8d4
balramMEL <balrammelkunde1999@gmail.com>
2025-01-13 17:32:49 +0530

    trail

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -23,12 +23,12 @@ app.use(cors({origin: "http://localhost:5173",credentials: true}));
 app.use("/api/user", UserRoute);
 app.use("/api/events", EventRoute);
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname,'../Frontend/dist')));
-    app.get('*',(req,res) => {
-        res.sendFile(path.join(__dirname,"../Frontend","dist","index.html"));
-    })
-}
+// if(process.env.NODE_ENV === 'production'){
+//     app.use(express.static(path.join(__dirname,'../Frontend/dist')));
+//     app.get('*',(req,res) => {
+//         res.sendFile(path.join(__dirname,"../Frontend","dist","index.html"));
+//     })
+// }
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
