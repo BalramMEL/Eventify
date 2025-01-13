cb88f3931a00a0a58e8cd3a885e2d66c5cfc1659
balramMEL <balrammelkunde1999@gmail.com>
2025-01-14 00:30:51 +0530

    trail6

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -23,12 +23,12 @@ app.use(cors({origin: process.env.NODE_ENV === "production" ? "https://eventify-
 app.use("/api/user", UserRoute);
 app.use("/api/events", EventRoute);
 
-// if(process.env.NODE_ENV === 'production'){
-//     app.use(express.static(path.join(__dirname,'../Frontend/dist')));
-//     app.get('*',(req,res) => {
-//         res.sendFile(path.join(__dirname,"../Frontend","dist","index.html"));
-//     })
-// }
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname,'../Frontend/dist')));
+    app.get('*',(req,res) => {
+        res.sendFile(path.join(__dirname,"../Frontend","dist","index.html"));
+    })
+}
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
