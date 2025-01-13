1b63c65e8e4c4a5b99b282caa5812519289a6b20
balramMEL <balrammelkunde1999@gmail.com>
2025-01-13 23:43:20 +0530

    trail5

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -23,11 +23,11 @@ app.use(cors({origin: process.env.NODE_ENV === "production" ? "https://eventify-
 app.use("/api/user", UserRoute);
 app.use("/api/events", EventRoute);
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname,'../Frontend/dist')));
-    app.get('*',(req,res) => {
-        res.sendFile(path.join(__dirname,"../Frontend","dist","index.html"));
-    })
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../Frontend/dist/index.html"));
+    });
 }
 
 server.listen(PORT, () => {
