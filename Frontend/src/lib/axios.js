5cddaa1d8e3f52df154a9cf25a0f6b010bb77da5
balramMEL <balrammelkunde1999@gmail.com>
2025-01-14 00:03:39 +0530

    trail5

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development"?  "https://eventify-zkl4.onrender.com/api":"/api",
-    // baseURL: import.meta.env.MODE === "development"?  "http://localhost:3000/api":"/api",
+    // baseURL: import.meta.env.MODE === "development"?  "https://eventify-zkl4.onrender.com/api":"/api",
+    baseURL: import.meta.env.MODE === "development"?  "http://localhost:3000/api":"https://eventify-zkl4.onrender.com/api",
     withCredentials: true,
 })
